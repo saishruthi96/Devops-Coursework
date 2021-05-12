c3a9d822a2e88535221d8e0dcffc02dfca233b95
saishruthi96 <saishruthimadhuri.kara@gmail.com>
2021-05-12 19:47:20 -0400

    Milestone Work Copy

--- HW5/Caches/basics/routes/hello.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-
-routes.get('/', (req, res) => {
-  res.send("🌏  Hello World‼️");
-});
-
-module.exports = routes;
--- HW5/Caches/basics/routes/simple.js
+++
@@ -1,9 +0,0 @@
-
-const routes = require('express').Router();
-
-routes.get("/dayofweek",(req,res)=>{
-    var dt = new Date();
-    res.send(dt.getDay().toString());
-   });
-
-module.exports = routes;
