c08ec9d3e12e80a1d9e23a4b1dfcd4f20d50af34
saishruthi96 <saishruthimadhuri.kara@gmail.com>
2021-05-12 19:49:30 -0400

    Milestone2 Work Copy

---
+++ Milestone2/commands/setup.js
@@ -0,0 +1,89 @@
+const child = require('child_process');
+const chalk = require('chalk');
+const path = require('path');
+const os = require('os');
+var fs = require('fs'); 
+
+
+const scpSync = require('../lib/scp');
+const sshSync = require('../lib/ssh');
+const { clear } = require('console');
+const filePath = "/bakerx/cm/playbook.yml";
+const inventoryPath = "/bakerx/cm/inventory.ini";
+
+exports.command = 'setup';
+exports.desc = 'Provision and configure the configuration server';
+exports.builder = yargs => {
+    yargs.options({
+        privateKey: {
+            describe: 'Install the provided private key on the configuration server',
+            type: 'string'
+        },
+        'gh-user': {
+            describe: 'GitHub username',
+            type: 'string',
+          },
+          'gh-pass': {
+            describe: 'GitHub password',
+            type: 'string',
+          },
+    });
+};
+
+
+exports.handler = async argv => {
+    const { privateKey , ghUser, ghPass} = argv;
+    const filepath = '.vault-pass';   
+
+    if (fs.existsSync(filepath)) {
+      
+        (async () => {
+
+            await run( privateKey ,ghUser, ghPass);
+    
+        })();
+    }
+    else{
+        console.log('Vault-pass file does not exist');
+    }
+   
+};
+
+async function run(privateKey,ghUser, ghPass) {
+    if (!ghUser || !ghPass) {
+        console.log("GITHUB credentials MISSING!!!");
+        process.exit(1);
+      }
+    console.log(chalk.greenBright('Installing configuration server!'));
+
+    console.log(chalk.blueBright('Provisioning configuration server...'));
+    let result = child.spawnSync(`bakerx`, `run config-srv --memory 4096 focal --ip 192.168.33.20 --sync`.split(' '), {shell:true, stdio: 'inherit'} );
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Loading privateKey on configuration server'));
+    let identifyFile = privateKey || path.join(os.homedir(), '.bakerx', 'insecure_private_key');
+    result = scpSync (identifyFile, 'vagrant@192.168.33.20:/home/vagrant/.ssh/mm_rsa');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Transfering vault-pass file on to the configuration server'));
+    result = scpSync ('.vault-pass', 'vagrant@192.168.33.20:/home/vagrant/.vault-pass');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright('Running init script...'));
+    result = sshSync('chmod +x /bakerx/cm/server-init.sh', 'vagrant@192.168.33.20');
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    result = sshSync("/bakerx/cm/server-init.sh", "vagrant@192.168.33.20");
+    if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+    console.log(chalk.blueBright("Running ansible script..."));
+
+  result = sshSync("chmod +x  /bakerx/cm/run-ansible.sh","vagrant@192.168.33.20");
+  if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+  result = sshSync("/bakerx/cm/run-ansible.sh","vagrant@192.168.33.20");
+  if( result.error ) { console.log(result.error); process.exit( result.status ); }
+
+result = sshSync(`ansible-playbook ${filePath} -i ${inventoryPath} --vault-password-file .vault-pass -e "git_username=${ghUser}" -e "git_password=${ghPass}"`,"vagrant@192.168.33.20");
+if( result.error ) { console.log(result.error); process.exit( result.status ); }
+}
